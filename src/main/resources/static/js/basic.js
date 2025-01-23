dacbf87ea5bb15e5d52c5ba23a50b543784cbb4d
hyper <revoluer9@gmail.com>
2025-01-23 22:28:21 +0900

    spring security 로그인

--- src/main/resources/static/js/basic.js
+++ src/main/resources/static/js/basic.js
@@ -3,7 +3,7 @@ let host = 'http://' + window.location.host;
 $(document).ready(function () {
     const auth = getToken();
     if(auth === '') {
-        //window.location.href = host + "/api/user/login-page";
+        window.location.href = host + "/api/user/login-page";
     } else {
         $('#login-true').show();
         $('#login-false').hide();
